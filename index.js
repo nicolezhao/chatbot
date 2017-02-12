594740fae47958e103dde652ce6c6019d9da4fc4
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-02-11 23:44:31 -0500

    try to fix json

--- index.js
+++ index.js
@@ -26,17 +26,15 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        if (event.message && event.message.text) {  
             if (!bearMessage(event.sender.id, event.message.text)) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-          } else if (event.postback) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
+        } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
-        }
         }
     }
     res.sendStatus(200);
-
 });
 
 // generic function sending messages
