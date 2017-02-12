eb4c0e15ffc92c9750073887861b564f7ccfe563
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-02-12 13:46:59 -0500

    change syntax receiving function

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {  
-            if (!bearMessage(event.sender.id, event.message.text)) {
+            if (!bearMessage(event.sender.id, event.message.text) && !TOweatherMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
             }
         } else if (event.postback) {
