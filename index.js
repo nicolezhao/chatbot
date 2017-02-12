1eaa457721f08ca99fbd6940a90364b90ea8a3f4
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-02-11 23:14:27 -0500

    try to fix postback

--- index.js
+++ index.js
@@ -26,13 +26,11 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
         if (event.message && event.message.text) {  
-          if (!bearMessage(event.sender.id, event.message.text)) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-          } else if (event.postback) {
+            if (!kittenMessage(event.sender.id, event.message.text)) {
+                sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            }
+        } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
         }
         }
