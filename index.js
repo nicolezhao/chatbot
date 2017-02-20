5bbb5c0580add31deb6c63c1635c8ae0f32b82c0
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-02-20 17:32:44 -0500

    change message

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook', function (req, res) {
             } else if (event.postback.payload == 'Like'){
                 sendMessage(event.sender.id, {text: "<3"});
             }
-        } else if (event.text.payload == "Toronto"){
+        } else if (event.text.payload == "San Fran"){
             sendMessage(event.sender.id, {text: "GOT IT!"});
         }
     }
