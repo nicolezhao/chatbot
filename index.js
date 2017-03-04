e4bf6b3bdc4273d0026f89b01cbf80ad07e24d95
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-03-04 15:38:13 -0500

    getForcast function test

--- index.js
+++ index.js
@@ -169,12 +169,20 @@ function outfitMessage(recipientId)  {
 };
 
 
+function getForcast(recepientId, city){
+    Weather.getCurrent("Kansas City", function(current) {
+      console.log(
+        ["currently:",current.temperature(),"and",current.conditions()].join(" ")
+      );
+    });
+}
 
 
 // send rich message 
 function weatherMessage(recipientId, text) {
 
     var city = text;
+    getForcast(recepientId, city);
 
     // if (values.length === 1 && values[0] === 'Toronto') {
 
