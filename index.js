c5273adcec2ab4df943646550d8ae193b2317a49
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-03-04 17:46:30 -0500

    get api key for weather

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var app = express();
 var weather = require('openweather-apis');
 
 weather.setLang('en');
+weather.setAPPID(b79ca3d3ebd382d195294ae0880dc596);
+weather.setUnits('metric');
 
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
