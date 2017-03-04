498fc88d204d32632d23324534c17d2a379acdfd
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-03-04 15:55:27 -0500

    again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');  
 var request = require('request');  
 var app = express();
-// var weather = require("weather.js");
+var weather = require("weather.js");
 
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
