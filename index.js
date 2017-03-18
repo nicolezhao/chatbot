f190dcc43211a8aa0c609da53d74568d0fe2baf0
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-03-18 14:28:55 -0400

    another test

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function initialMessage(recipientId)  {
     "text":"Send your location or type a city in:",
     "quick_replies":[
       {
-        "content_type":"text",
+        "content_type":"payload",
         "title":"Current Location",
         "payload":"location"
       },
