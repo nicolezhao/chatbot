51490cb490bb87b9a80830a959b14b2d7c38138d
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-03-18 15:15:28 -0400

    debug

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-
 // handler receiving messages
 app.post('/webhook', function (req, res) {  
     var events = req.body.entry[0].messaging;
